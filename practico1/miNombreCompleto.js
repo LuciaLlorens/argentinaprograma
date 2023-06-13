45561865c6ea6ec678ed96464bbcfa9c63976cb9
LuciaLlorens <luciallrns@gmail.com>
2023-06-13 20:48:08 -0300

    Agrego el archivo miNombreCompleto.js

---
+++ practico1/miNombreCompleto.js
@@ -0,0 +1,25 @@
+// Escribir un programa que:
+// a. Defina una variable nombre.
+
+let nombre;
+
+// b. Guarde en la variable nombre tu nombre real.
+
+nombre = "Lucía";
+
+// c. Defina y asigne, en un mismo paso, la variable apellido. El valor a asignar deberá ser tu apellido real.
+
+let apellido = "Llorens";
+
+// d. Repita el paso anterior, pero esta vez definiendo la variable edad y asignandole tu edad real.
+
+let edad = 23;
+
+// e. Por último, imprime por consola un mensaje que respete el siguiente formato:
+// "Hola, [nombre] [apellido]. Tienes [edad] años."
+
+console.log("Hola, %s %s. Tienes %d años.", nombre, apellido, edad);
+/* 
+Imprime por consola:
+Hola, Lucía Llorens. Tienes 23 años.
+*/
